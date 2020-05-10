7c514661cc66768a3561239c5781f713b6aef5ad
Mihawkkkk <pvkhai98dn@gmail.com>
2020-05-11 01:29:18 +0700

    Add search, login and logo

--- app.py
+++ app.py
@@ -30,5 +30,8 @@ def index():
         limage.append(sImage)
     return render_template("index.html", lname = lname, lrating = lrating, limage = limage) 
 
+@app.route("/login")
+def login(): 
+    return render_template("login.html")
 if __name__ == "__main__":
     app.run(debug=True)    
