75895e47dffc632f1053d9af89095fe4fb652cac
Mihawkkkk <pvkhai98dn@gmail.com>
2020-05-14 01:35:37 +0700

    Display 5 pages at a time; Add icon

--- app.py
+++ app.py
@@ -7,27 +7,29 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():  
-    url = "https://www.foody.vn/da-nang/nha-hang"
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    number = 12
-    journey = soup.findAll("div", class_='row-item filter-result-item')[:number]
-    rates = soup.findAll("div", class_='point highlight-text')[:number]
-    images = soup.findAll("div", class_='ri-avatar result-image')[:number]
-
     lname = []
     lrating = []
     limage = []
-    for i in range(0, len(journey)):
-        sName = journey[i].find("h2").text
-        sName.strip("\n\r\n")
-        lname.append(sName[43:len(sName) - 39])
-        sRating = rates[i].text
-        sRating.strip("\n\r\n")
-        sRating = float(sRating[38:len(sRating) - 32])
-        lrating.append(sRating)
-        sImage = images[i].find('img')['src']
-        limage.append(sImage)
+    for j in range(1,6):
+        url = "https://www.foody.vn/da-nang/nha-hang"
+        url = "https://www.foody.vn/da-nang/nha-hang?page=" + str(j) + "#!#page" + str(j)
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        number = 12
+        journey = soup.findAll("div", class_='row-item filter-result-item')[:number]
+        rates = soup.findAll("div", class_='point highlight-text')[:number]
+        images = soup.findAll("div", class_='ri-avatar result-image')[:number]
+
+        for i in range(0, len(journey)):
+            sName = journey[i].find("h2").text
+            sName.strip("\n\r\n")
+            lname.append(sName[43:len(sName) - 39])
+            sRating = rates[i].text
+            sRating.strip("\n\r\n")
+            sRating = float(sRating[38:len(sRating) - 32])
+            lrating.append(sRating)
+            sImage = images[i].find('img')['src']
+            limage.append(sImage)
     return render_template("index.html", lname = lname, lrating = lrating, limage = limage) 
 
 @app.route("/login")
--- getFoodySele.py
+++ getFoodySele.py
@@ -5,27 +5,26 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 option = webdriver.ChromeOptions()
-#option.add_argument('headless')
-
-# driver = webdriver.Chrome('./chromedriver',options=option)
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=option)
-print("sample test case started")
-#driver = webdriver.Chrome(options=)
 #driver=webdriver.firefox()
 #driver=webdriver.ie()
+print("sample test case started")
+
 #maximize the window size
 driver.maximize_window()
+
 #navigate to the url
 driver.get("https://www.foody.vn/da-nang/nha-hang")
-time.sleep(3)
-#click on the Google search button
-button = driver.find_element_by_xpath("/html/body/div[2]/section/div/div[2]/div/div/div/div/div[2]/div[5]/div[1]/div/div/div[1]/div[2]/div[1]/div[2]/h2/a")
+time.sleep(1)
 
-button.click()
-time.sleep(20)
+button = driver.find_element_by_xpath("/html/body/div[2]/section/div/div[2]/div/div/div/div/div[2]/div[8]/a")
+
+for i in range(5):
+    button.click()
+    time.sleep(1)
 #Number = driver.find_element_by_xpath("/html/body/div[20]/div/div[1]/div/div[1]/div[3]/div[2]/ul/li[1]/div[1]").text
 #print("Số người nhiễm: ", Number)
 #print(driver.find_element_by_class_name("statistic_number red"))
-#close the browser
-#driver.close()
-#print("sample test case successfully completed")
+# close the browser
+# driver.close()
+# print("sample test case successfully completed")
