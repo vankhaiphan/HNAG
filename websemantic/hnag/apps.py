befd7528419f15a395e4a9c7f8c162382bac41c8
Mihawkkkk <pvkhai98dn@gmail.com>
2020-05-25 02:00:13 +0700

    Add login logout function

---
+++ websemantic/hnag/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HnagConfig(AppConfig):
+    name = 'hnag'
---
+++ websemantic/hnag/views.py
@@ -0,0 +1,27 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+# Create your views here.
+def index(request):
+    if not request.user.is_authenticated:
+        return render(request, "hnag/login.html", {"message": None})
+    context = {
+        "user": request.user
+    }
+    return render(request, "hnag/user.html", context)
+
+def login_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "hnag/login.html", {"message": "Invalid credentials."})
+
+def logout_view(request):
+    logout(request)
+    return render(request, "hnag/login.html", {"message": "Logged out."})
